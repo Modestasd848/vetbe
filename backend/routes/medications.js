851931048df0824260c958b662a3fe9ccd2c1e36
Modestasd848 <Modestasd848@gmail.com>
2023-03-11 22:18:36 +0200

    router add

--- backend/routes/medications.js
+++ backend/routes/medications.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import {
+  createNewMed,
+  getAllMed,
+} from '../controllers/medicationController.js';
 const router = express.Router();
 
-router.get('/medications');
-router.post('/medications');
+router.get('/medications', getAllMed);
+router.post('/medications', createNewMed);
 
 export default router;
